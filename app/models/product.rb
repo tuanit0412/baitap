0ba2edb55a19fdd754e27dac31a1b7b3a41186d9
Pete <namtrung1810it.gmail.com>
2018-05-22 17:50:48 +0700

    Validate title, desciption,image

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+	validates :title, :description,:image_url, presence: true #validate khong cho null
+	validates :title , uniqueness: true
+	validates :price, numericality: {greater_than_or_equal: 0.01, message:"Gia phai la so"}
+	validates :image_url, allow_blank: true, format: {with:%r{\.(gif|jpg|png)\Z}i,message: 'file anh phai co duoi la .jpg, .png, .gif'}
 end
